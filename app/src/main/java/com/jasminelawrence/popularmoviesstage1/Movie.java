986294fe685dd7f11207a309995bc5824f363ddd
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-05-08 01:26:52 -0700

    Create Movie object with required member vars

---
+++ app/src/main/java/com/jasminelawrence/popularmoviesstage1/Movie.java
@@ -0,0 +1,62 @@
+package com.jasminelawrence.popularmoviesstage1;
+
+
+public class Movie {
+
+    private String originalTitle;
+    private String posterImage;
+    private String plotSynopsis;
+    private float userRating;
+    private String releaseDate;
+
+
+    public Movie(String originalTitle, String posterImage, String plotSynopsis, float userRating, String releaseDate) {
+        this.originalTitle = originalTitle;
+        this.posterImage = posterImage;
+        this.plotSynopsis = plotSynopsis;
+        this.userRating = userRating;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getPosterImage() {
+        return posterImage;
+    }
+
+    public void setPosterImage(String posterImage) {
+        this.posterImage = posterImage;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+
+    public void setPlotSynopsis(String plotSynopsis) {
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    public float getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(float userRating) {
+        this.userRating = userRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+
+}
