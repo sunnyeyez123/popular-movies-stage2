b9d294851d8423a9cc6ddbb40369112e326e5f32
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-05-09 22:07:23 -0700

    create first pass custom movie adapter

---
+++ app/src/main/java/com/jasminelawrence/popularmoviesstage1/MovieAdapter.java
@@ -0,0 +1,49 @@
+package com.jasminelawrence.popularmoviesstage1;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MovieAdapter  extends ArrayAdapter<Movie> {
+    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+
+
+    public MovieAdapter(Activity context, List<Movie> movies) {
+        super(context, 0, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the movie from the ArrayAdapter at the selected position
+        Movie movie = getItem(position);
+
+        // Adapters recycle views to AdapterViews.
+        // If this is a new View object we're getting, then inflate the layout.
+        // If not, this view already has the layout inflated from a previous call to getView,
+        // and we modify the View widgets as usual.
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_list_item, parent, false);
+        }
+
+        ImageView iconView = convertView.findViewById(R.id.image_iv);
+        Picasso.with(getContext())
+                .load(movie.getPosterImage())
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .into(iconView);
+
+        TextView versionNameView = convertView.findViewById(R.id.original_title_tv);
+        versionNameView.setText(movie.getOriginalTitle());
+
+
+        return convertView;
+    }
+}
