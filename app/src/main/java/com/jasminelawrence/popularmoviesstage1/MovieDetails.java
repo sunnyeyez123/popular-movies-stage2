8dcce9e1dd9daa43e94399170bad03b25d4f6552
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-05-09 23:23:06 -0700

    only look for extra data if the extras bundle isn't empty

--- app/src/main/java/com/jasminelawrence/popularmoviesstage1/MovieDetails.java
+++ app/src/main/java/com/jasminelawrence/popularmoviesstage1/MovieDetails.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MovieDetails extends AppCompatActivity {
 
@@ -14,16 +15,20 @@ public class MovieDetails extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras == null) {
-            //tbd
-        }
-// get data via the key
-        String name = extras.getString("ORIGINAL_NAME");
-        if (name != null) {
-            // do something with the data
+            Toast.makeText(MovieDetails.this, "No movie data", Toast.LENGTH_SHORT).show();
 
-            TextView nameTextView = findViewById(R.id.original_name_tv);
-            nameTextView.setText(name);
-            setTitle(name);
+        }else{
+            // get data via the key
+            String name = extras.getString("ORIGINAL_NAME");
+            if (name != null) {
+                // do something with the data
+
+                TextView nameTextView = findViewById(R.id.original_name_tv);
+                nameTextView.setText(name);
+                setTitle(name);
+            }
+
         }
+
     }
 }
