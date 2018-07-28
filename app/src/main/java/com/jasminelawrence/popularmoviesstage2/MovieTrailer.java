a0d0cde6ff12bad5b1b67b24dec64dd2da573576
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-07-28 15:37:28 -0700

    Create movie trailer class

---
+++ app/src/main/java/com/jasminelawrence/popularmoviesstage2/MovieTrailer.java
@@ -0,0 +1,21 @@
+package com.jasminelawrence.popularmoviesstage2;
+
+public class MovieTrailer {
+
+    private String mName;
+    private String mKey;
+
+    public MovieTrailer(String name, String key){
+        mName = name;
+        mKey = key;
+
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmKey() {
+        return mKey;
+    }
+}
